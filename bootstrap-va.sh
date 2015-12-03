cf0d5549345e841a9cc29f4411b7607543a29aa9
root <root@va-monitoring.novalocal>
2015-12-03 14:56:30 +0000

    Modify bootstrap script for changes in salt repo and file tree

--- bootstrap-va.sh
+++ bootstrap-va.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-export SALTTREE=salt-tree.tar.gz
-wget -O - https://repo.saltstack.com/apt/debian/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
-echo 'deb http://repo.saltstack.com/apt/debian jessie contrib' > /etc/apt/sources.list.d/salt.list
+#export SALTTREE=salt-tree.tar.gz
+wget -O - https://repo.saltstack.com/apt/debian/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
+echo 'deb http://repo.saltstack.com/apt/debian/latest jessie main' > /etc/apt/sources.list.d/salt.list
 apt-get update
 apt-get install --no-install-recommends salt-master
-tar xzfv $SALTTREE -C /srv
-cp /srv/salt/state/monitoring/files/master /etc/salt/master
+#tar xzfv $SALTTREE -C /srv
+mkdir /srv/{salt,pillar}
+cp -R states/* /srv/salt
+cp -R pillars/* /srv/pillar
+#setup pillars
+cp /srv/salt/salt-master/files/master /etc/salt/master
 service salt-master restart
 apt-get install salt-minion
 echo "master: $(hostname -f)" >> /etc/salt/minion
-echo "role: monitoring" >> /etc/salt/grains
+echo "role: va-monitoring" >> /etc/salt/grains
 service salt-minion restart
+salt-key -y -a va-monitoring.novalocal
 salt '*' state.highstate
