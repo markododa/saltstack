92506e7feca1529511c74adc07d98b32152d6854
Marko Doda <marko@va.mk>
2016-01-20 15:37:17 +0100

    get VPN config with file

--- modules/openvpn.py
+++ modules/openvpn.py
@@ -1,8 +1,33 @@
 import salt
 
 def add_user(username):
-	__salt__['cmd.run']('/etc/openvpn/easyrsa build-client-full '+username+' nopass',cwd='/etc/openvpn')
+	if __salt__['cmd.retcode']('/etc/openvpn/easyrsa build-client-full '+username+' nopass',cwd='/etc/openvpn') == 0:
+		return True
+	else:
+		return False
 
 def revoke_user(username):
-	__salt__['cmd.run']('echo yes | /etc/openvpn/easyrsa revoke '+username,cwd='/etc/openvpn',python_shell=True)
-	__salt__['cmd.run']('/etc/openvpn/easyrsa gen-crl',cwd='/etc/openvpn')
+	if __salt__['cmd.retcode']('echo yes | /etc/openvpn/easyrsa revoke '+username,cwd='/etc/openvpn',python_shell=True) == 0:
+		__salt__['cmd.run']('/etc/openvpn/easyrsa gen-crl',cwd='/etc/openvpn')
+		return True
+	else:
+		return False
+
+def list_users():
+	certs = open('/etc/openvpn/pki/index.txt', 'r')
+	next(certs)
+	active = ''
+	revoked = ''
+	for line in certs:
+		if line[:1] == 'V':
+			active = active + 'Active: ' + line.split('CN=')[1]
+		elif line[:1] == 'R':
+			revoked = revoked + 'Revoked: ' + line.split('CN=')[1]
+	return active+revoked
+
+def get_config(username):
+	config = open('/etc/openvpn/client.conf','r').read()
+	ca = open('/etc/openvpn/pki/ca.crt', 'r').read()
+	cert = open('/etc/openvpn/pki/issued/'+username+'.crt','r').read().split('-----BEGIN CERTIFICATE-----\n')[1]
+	key = open('/etc/openvpn/pki/private/'+username+'.key','r').read()
+	return config+'\n<ca>\n'+ca+'</ca>'+'\n<cert>\n-----BEGIN CERTIFICATE-----\n'+cert+'</cert>\n'+'<key>\n'+key+'</key>'
