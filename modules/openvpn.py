7fe8d0e53e354dbab28f778aeff2742c3e8cd149
Marko Doda <marko@va.mk>
2016-01-26 15:53:18 +0100

    Fix revoke

--- modules/openvpn.py
+++ modules/openvpn.py
@@ -1,36 +1,37 @@
-import salt
+import salt, vpn_parser
 
 def add_user(username):
-	if __salt__['cmd.retcode']('/etc/openvpn/easyrsa build-client-full '+username+' nopass',cwd='/etc/openvpn') == 0:
+	if __salt__['cmd.retcode']('/etc/openvpn/easyrsa/easyrsa build-client-full '+username+' nopass',cwd='/etc/openvpn/easyrsa') == 0:
 		return True
 	else:
 		return False
 
 def revoke_user(username):
-	if __salt__['cmd.retcode']('echo yes | /etc/openvpn/easyrsa revoke '+username,cwd='/etc/openvpn',python_shell=True) == 0:
-		__salt__['cmd.run']('/etc/openvpn/easyrsa gen-crl',cwd='/etc/openvpn')
+	if __salt__['cmd.retcode']('echo yes | /etc/openvpn/easyrsa/easyrsa revoke '+username,cwd='/etc/openvpn/easyrsa',python_shell=True) == 0:
+		__salt__['cmd.run']('/etc/openvpn/easyrsa/easyrsa gen-crl',cwd='/etc/openvpn/easyrsa')
+                __salt__['cmd.run']('cp /etc/openvpn/easyrsa/pki/crl.pem /etc/openvpn/crl.pem && chown nobody:nogroup /etc/openvpn/crl.pem')
 		return True
 	else:
 		return False
 
 def list_users():
-	certs = open('/etc/openvpn/pki/index.txt', 'r')
+	certs = open('/etc/openvpn/easyrsa/pki/index.txt', 'r')
 	next(certs)
-	active = ''
-	revoked = ''
+	active = []
+	revoked = []
 	for line in certs:
 		if line[:1] == 'V':
-			active = active + 'Active: ' + line.split('CN=')[1]
+			active = active + [line.split('CN=')[1].strip()]
 		elif line[:1] == 'R':
-			revoked = revoked + 'Revoked: ' + line.split('CN=')[1]
-	return active+revoked
+			revoked = revoked + [line.split('CN=')[1].strip()]
+	return {'active' : active, 'revoked' : revoked}
 
 def get_config(username):
 	config = open('/etc/openvpn/client.conf','r').read()
-	ca = open('/etc/openvpn/pki/ca.crt', 'r').read()
-	cert = open('/etc/openvpn/pki/issued/'+username+'.crt','r').read().split('-----BEGIN CERTIFICATE-----\n')[1]
-	key = open('/etc/openvpn/pki/private/'+username+'.key','r').read()
+	ca = open('/etc/openvpn/easyrsa/pki/ca.crt', 'r').read()
+	cert = open('/etc/openvpn/easyrsa/pki/issued/'+username+'.crt','r').read().split('-----BEGIN CERTIFICATE-----\n')[1]
+	key = open('/etc/openvpn/easyrsa/pki/private/'+username+'.key','r').read()
 	return config+'\n<ca>\n'+ca+'</ca>'+'\n<cert>\n-----BEGIN CERTIFICATE-----\n'+cert+'</cert>\n'+'<key>\n'+key+'</key>'
 
 def get_status():
-	return open('/run/openvpn/openvpn-status.log','r').read()
+	return vpn_parser.open_and_parse_log('/run/openvpn/openvpn-status.log')
