ed8c7beda77ff77c15e0940a519f730dc0a876ef
Vlatko <vlatko@va.mk>
2016-03-01 12:21:45 +0100

    WinTools

---
+++ states/fileshare/wintools/mp.bat
@@ -0,0 +1,12 @@
+﻿@echo off
+set DOM=DOMEJN
+net use * /DELETE /Y >nul 2>&1
+echo ## Enter username in format: %DOM%\username  
+echo #########################################################
+REM Replace files with ip of va-fileshare or edit hosts
+net use P: \\files\Personal /SAVECRED 
+net use S: \\files\Share\ /SAVECRED 
+net use X: \\files\Public
+
+echo ## Done!  
+pause
