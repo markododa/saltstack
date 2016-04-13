e0a9f2f56b7f88e3fcc2f2bb5d606fff2ae1938f
root <root@va-monitoring.novalocal>
2016-04-13 08:51:23 +0000

    samba

--- modules/samba.py
+++ modules/samba.py
@@ -2,4 +2,10 @@
 ''' Imports all VapourApps API functions into SaltStack'''
 import salt, sys
 from va_samba_api import *
+import samba_parser
 
+def users_last_logins():
+    return samba_parser.open_and_parse_log('/var/log/lastlogin.log')
+
+def users_log():
+    return samba_parser.open_and_parse_log('/var/log/user.log')
