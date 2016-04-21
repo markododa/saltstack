569d910fd8acd76284921bcf0c2616479b62869e
root <root@va-monitoring.WAPORZ.MK>
2016-04-21 08:03:59 +0000

    Samba lastlogin fix

--- states/directory/files/update.sh
+++ states/directory/files/update.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-user="$(echo $1| sed 's/.*smbd.*\\//' | cut -d'|' -f1)"
-exist=$(cat /var/log/lastlogin.log | grep $(echo $1| sed 's/.*smbd.*\\//' | cut -d'|' -f1)| wc -c)
-if [ "$exist" = "0" ]; then
-#echo "new user"
-echo "Jan  1 00:00:01 va-temp smbd_audit: TEMP\"$user""|8.8.8.8|connect|ok|sysvol">> /var/log/lastlogin.log
+user=`echo $1 | sed -e s/.*smbd_audit\:\ // -e s/\|.*// -e s'#\\\#\\\\\\\#'`
+string=`echo $1 | sed s'#\\\#\\\\\\\#'`
+echo $user
+if grep -q "$user" /var/log/lastlogin.log ; then
+  sed -i s"/^.*$user.*$/$string/g" /var/log/lastlogin.log
+else
+  echo $1 >> /var/log/lastlogin.log
 fi
-#echo "replacing"
-sed -i s"/.*\\""$(echo $1| sed 's/.*smbd.*\\//' | cut -d'|' -f1)""|.*/\\""$(echo $1| sed 's/\\/\\\\/')|""/" /var/log/lastlogin.log
-
