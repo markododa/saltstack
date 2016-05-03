6ef5fb87d8d9f25ac29258e0f3411641b443ff04
Vlatko <vlatko@va.mk>
2016-05-03 10:53:32 +0200

    disable samba syslog logging (duplicate)

---
+++ states/directory/files/fsmo.sh
@@ -0,0 +1,11 @@
+﻿#! /bin/sh
+ldbsearch --cross-ncs -H /var/lib/samba/private/sam.ldb '(fsmoroleowner=*)' | grep 'dn:\|Role' | sed 's/CN=//g' |\
+ sed 's/,Servers,.*//'|\
+ sed 's/NTDS Settings,//'|\
+ sed 's/fSMORoleOwner: //'|\
+ sed 's/dn: //'|\
+ sed 's/DC=//g'
+
+ 
+echo "samba-tool fsmo transfer --role=all"
+samba-tool fsmo show
