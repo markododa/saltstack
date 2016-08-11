6739aa82451019289eb6c880bcf574f5db05f0b7
Vlatko <vlatko@va.mk>
2016-08-11 08:55:21 +0200

    Signed-off-by: Vlatko <vlatko@va.mk>

---
+++ states/owncloud/files/test.php
@@ -0,0 +1,13 @@
+<?php
+$AUTOCONFIG = array(
+  "dbtype"        => "mysql",
+  "dbname"        => "DB_NAME",
+  "dbuser"        => "DB_USER",
+  "dbpass"        => "DB_PASS",
+  "dbhost"        => "localhost",
+  "dbtableprefix" => "",
+  "adminlogin"    => "USER",
+  "adminpass"     => "PASS",
+  "directory"     => "/var/www/owncloud/data/",
+  "theme"         => "va-theme",
+);
