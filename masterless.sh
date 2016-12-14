96ce7cd0dafecb7346f2c84010a54bc5d51289a8
Marko Doda <marko@lugola.net>
2016-12-14 12:34:31 +0100

    fix if condition

--- masterless.sh
+++ masterless.sh
@@ -3,13 +3,20 @@ set -e
 if [ -z "$1" ]
 then 
 echo "The first argument should be the role"
+exit
 fi
 if [ -n "$1" ]
 then
 ROLE=$1
+fi
 if [ `lsb_release -cs` == "jessie" ]; then
 wget -O - https://repo.saltstack.com/apt/debian/8/amd64/latest/SALTSTACK-GPG-KEY.pub | apt-key add -
-echo 'deb http://repo.saltstack.com/apt/debian/8/amd64/latest jessie main' > /etc/apt/sources.list.d/salt.list; fi
+echo 'deb http://repo.saltstack.com/apt/debian/8/amd64/latest jessie main' > /etc/apt/sources.list.d/salt.list
+fi
+if [ `lsb_release -cs` == "xenial" ]; then
+wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | apt-key add -
+echo 'deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main' > /etc/apt/sources.list.d/salt.list
+fi
 apt-get update -y
 apt-get install salt-minion -y
 cd "$(dirname "$0")"
@@ -23,3 +30,4 @@ echo "role: $ROLE" > /etc/salt/grains
 service salt-minion restart
 #setup pillars
 echo "Setup pillars and run salt-call --local state.highstate or salt-call --local state.apply"
+
