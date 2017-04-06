c8ce4ae1a4a406d10790ca15797631fc480e2b88
Marko Doda <doda@lemon.local>
2017-04-06 12:22:30 +0200

    fix firewall nat rules to use part as argument

--- modules/firewall.py
+++ modules/firewall.py
@@ -11,11 +11,11 @@ def grep_nat(port):
 def insert_nat(destination, port):
     destination=str(destination)
     port=str(port)
-    rule='-i eth0 -p tcp -d 10.107.150.20 --dport '+port+' -j DNAT --to-destination '+destination+':3389'
+    rule='-i eth0 -p tcp -d 10.107.150.20 --dport '+port+' -j DNAT --to-destination '+destination+':'+port
     return __salt__['iptables.insert']('nat', 'PREROUTING', position=1, rule=rule)
 
 def delete_nat(destination, port):
     destination=str(destination)
     port=str(port)
-    rule='-i eth0 -p tcp -d 10.107.150.20 --dport '+port+' -j DNAT --to-destination '+destination+':3389'
+    rule='-i eth0 -p tcp -d 10.107.150.20 --dport '+port+' -j DNAT --to-destination '+destination+':'+port
     return __salt__['iptables.delete']('nat', 'PREROUTING', rule=rule)
