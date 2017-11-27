783e2ac6e3aee162c3615aa7997925bf0a970813
staci <staci@va.mk>
2017-11-27 14:38:36 +0000

    UPdated latest backuppc version.

--- modules/backuppc.py
+++ modules/backuppc.py
@@ -4,19 +4,19 @@ sshcmd='ssh -oStrictHostKeyChecking=no root@'
 rm_key='ssh-keygen -f "/var/lib/backuppc/.ssh/known_hosts" -R '
 
 default_paths = {
-    'va-monitoring' : ['/root/.va/backup', '/etc/icinga2', '/etc/ssmtp', '/usr/lib/nagios/plugins', '/var/lib/pnp4nagios/perfdata'],
-    'va-directory' : ['/root/.va/backup', '/home', '/var/lib/samba', '/etc/openvpn'],
-    'va-backup' : ['/root/.va/backup','/etc/backuppc'],
-    'va-fileshare' : ['/root/.va/backup','/home', '/etc/samba'],
-    'va-email' : ['/root/.va/backup', '/etc/postfix',  '/var/vmail'],
+    'va-monitoring' : ['/etc/icinga2', '/root/.va/backup', '/var/lib/pnp4nagios/perfdata/'],
+    'va-directory' : ['/root/.va/backup', '/etc/openvpn'],
+    'va-backup' : ['/etc/backuppc'],
+    'va-fileshare' : ['/home', '/etc/samba'],
+    'va-email' : ['/etc/postfix', '/root/.va/backup', '/var/vmail/'],
     'va-owncloud' : ['/root/.va/backup', '/var/www/owncloud'],
-    'va-proxy' : ['/root/.va/backup', '/etc/e2guardian', '/var/www/html', '/etc/lighttpd', '/etc/squid'], 
-    'va-ticketing' : ['/root/.va/backup', '/etc/dbconfig-common/redmine/instances', '/usr/share/redmine', '/var/www/html/redmine'], 
 }
 
 panel = {"backup.manage": {"title":"All backups","tbl_source":{"table":{}},"content":[{"type":"Form","name":"form","class":"tbl-ctrl","elements":[{"type":"Button","name":"Add Backup","glyph":"plus","action":"modal","reducers":["modal"],"modal":{"title":"Add a backup","buttons":[{"type":"Button","name":"Cancel","action":"cancel"},{"type":"Button","name":"Add backup","class":"primary","action":"add_folder"}],"content":[{"type":"Form","name":"form","class":"left","elements":[{"type":"text","name":"hostname","value":"","label":"App","required":True},{"type":"text","name":"backup_path","value":"","label":"Backup path","required":True}]},{"type":"Div","name":"div","class":"right","elements":[{"type":"Heading","name":"Fill the form to add a new backup"},{"type":"Paragraph","name":"Enter the full absolute path to the backup. The file must exist."}]},]}}]},{"type":"Table","name":"table","reducers":["table","panel","alert"],"subpanels":{"link":"backup.info"},"columns":[{"key":"app","label":"App","action":"all:link","colClass":"link"},{"key":"path","label":"Path","width":"60%"},{"key":"action","label":"Actions"}],"actions":[{"action":"rm_folder","name":"Remove"}],"id":["app","path"]}]}, "backup.browse": {"title":"Browse backups","tbl_source":{"table":{}},"content":[{"type":"Path","name":"path","action":"dir_structure1","target":"table","reducers":["table","panel"]},{"type":"Form","name":"form","target":"table","reducers":["panel","alert","table","form"],"class":"tbl-ctrl tbl-ctrl-dropdown","elements":[{"type":"dropdown","name":"dropdown","action":"dir_structure1"}]},{"type":"Table","name":"table","reducers":["table","panel","alert"],"columns":[{"key":"dir","label":"Files","width":"30%","action": "folder:dir_structure1", "colClass": "type"},{"key":"size","label":"Size"},{"key":"time","label":"Time"},{"key":"action","label":"Actions"}],"actions":[{"action":"rm_folder","name":"Remove"},{"action":"restore","name":"Restore"},{"action":"restore_backup","name":"Restore to Host"},{"action":{"type":"download","name":"download_zip"},"name":"Download"}],"id":["dir"]}]}, "backup.info": {"title":"Backup info","tbl_source":{"table":{}},"content":[{"type":"Table","name":"table","reducers":["table","panel","alert"],"panels":{"link":"backup.browse"},"columns":[{"key":"age","label":"Age"},{"key":"backup","label":"Backup num","action":"all:link","colClass":"link"},{"key":"duration","label":"Duration"},{"key":"startTime","label":"Start time"},{"key":"endTime","label":"End time"},{"key":"type","label":"Type"}],"id":["link"]}]} }
 
-def get_panel(panel_name, host = '', backupNum = -1):
+#def get_panel(panel_name, host = '', backupNum = -1):
+def get_panel(panel_name, server_name = '', backupNum = -1):
+    host = server_name
     ppanel = panel[panel_name]
     hostnames = __salt__['backuppc.listHosts']()
     if panel_name == "backup.manage":
@@ -60,7 +60,8 @@ def dir_structure1(host, *args):
         if key in attrib:
             a = attrib[key]
             size, time = (a['size'], a['time'])
-        result.append({'dir': key, 'type': type, 'size': size, 'time': time});
+        if size != '/' and time != '/': 
+            result.append({'dir': key, 'type': type, 'size': size, 'time': time});
     if check:
         return {'val': backupNum, 'list': result}
     return result
@@ -83,19 +84,14 @@ def add_default_paths(hosts = []):
             result = add_folder(host, path)
     return True
 
-def test_mine(hostname):
-	address = __salt__['mine.get']('fqdn:'+hostname,'address',expr_form='grain')
-	return address[address.keys()[0]]
-
 def hosts_file_add(hostname, address=False):
     hostname = hostname.lower()
     if not address:
         address = __salt__['mine.get']('fqdn:'+hostname,'address',expr_form='grain')
         address = address[address.keys()[0]][0]
     __salt__['file.append']('/etc/hosts',address+'\t'+hostname)
-    return address
 
-def add_host(hostname,address=False,script_pre="None",script_post="None"):
+def add_host(hostname,address=False,scriptpre="None",scriptpost="None"):
 	hostname = hostname.lower()
 	if not __salt__['file.file_exists']('/etc/backuppc/pc/'+hostname+'.pl'):
 		hosts_file_add(hostname,address)
@@ -106,10 +102,10 @@ def add_host(hostname,address=False,script_pre="None",script_post="None"):
         	__salt__['event.send']('backuppc/copykey', fqdn=hostname)
                 __salt__['cmd.retcode'](cmd=rm_key+hostname, runas='backuppc', shell='/bin/bash',cwd='/var/lib/backuppc')
                 __salt__['cmd.retcode'](cmd=sshcmd+hostname+' exit', runas='backuppc', shell='/bin/bash',cwd='/var/lib/backuppc')
-		if script_pre != "None":
-			__salt__['file.append']('/etc/backuppc/pc/'+hostname+'.pl','$Conf{DumpPreUserCmd} = \'$sshPath -q -x -l root $host '+script_pre+'\';')
-		if script_post != "None":
-			__salt__['file.append']('/etc/backuppc/pc/'+hostname+'.pl','$Conf{DumpPostUserCmd} = \'$sshPath -q -x -l root $host '+scripti_post+'\';')
+		if scriptpre != "None":
+			__salt__['file.append']('/etc/backuppc/pc/'+hostname+'.pl','$Conf{DumpPreUserCmd} = \'$sshPath -q -x -l root $host '+scriptpre+'\';')
+		if scriptpost != "None":
+			__salt__['file.append']('/etc/backuppc/pc/'+hostname+'.pl','$Conf{DumpPostUserCmd} = \'$sshPath -q -x -l root $host '+scriptpost+'\';')
 	return True
 
 # $Conf{ClientCharset} = 'cp1252';
@@ -132,12 +128,12 @@ def rm_host(hostname):
     __salt__['file.line'](path='/etc/hosts',content='.*'+hostname+'.*', mode='delete')
     return __salt__['service.reload']('backuppc')
 
-def add_folder(hostname, folder,script_pre="None",script_post="None",address=False):
+def add_folder(hostname, folder,address=False,scriptpre="None",scriptpost="None"):
     hostname = hostname.lower()
     if folder[-1] == '/':
         folder = folder[0:-1]
     if not __salt__['file.file_exists']('/etc/backuppc/pc/'+hostname+'.pl'):
-	    add_host(hostname,address,script_pre,script_post)
+	    add_host(hostname,address,scriptpre,scriptpost)
     if __salt__['file.search']('/etc/backuppc/pc/'+hostname+'.pl','\''+folder+'/?\''):
 		return False
     elif __salt__['cmd.retcode'](cmd=sshcmd+hostname+' test ! -d '+folder, runas='backuppc', shell='/bin/bash',cwd='/var/lib/backuppc'):
@@ -147,9 +143,9 @@ def add_folder(hostname, folder,script_pre="None",script_post="None",address=Fal
     else:
         return False
 
-def add_folder_list(hostname, folder_list,script_pre="None", script_post="None", address=False):
+def add_folder_list(hostname, folder_list,script="None"):
 	for folder in folder_list:
-		add_folder(hostname, folder,script_pre, script_post, address)
+		add_folder(hostname, folder,script)
 
 def rm_folder(hostname, folder):
     hostname = hostname.lower()
@@ -328,23 +324,73 @@ def tar_create(arguments, location='/usr/share', backupname='test_backup', backu
     tar_create_cmd = '/usr/share/backuppc/bin/BackupPC_tarCreate -h '+arguments[0]+' -s '+arguments[1]+' -n '+str(backupnumber)+' '+arguments[2]+' > '+location+'/'+backupname+'.tar'
     return __salt__['cmd.run'](tar_create_cmd ,runas='backuppc', cwd='/var/lib/backuppc',python_shell=True)
 
-def download_zip(hostname, share, path, backupnumber=-1):
-    zip_create_cmd = '/usr/share/backuppc/bin/BackupPC_zipCreate'
-    args = ' -h '+hostname+' -n '+str(backupnumber)+' -s '+share+' '+path
-    return __salt__['cmd.run'](zip_create_cmd+args,runas='backuppc', cwd = '/usr/lib/backuppc/')
+def fake_download(range_from):
+    class A(object): pass
+    p = A()
+    import StringIO
+    p.stdout = StringIO.StringIO('zdravo jas sum filip')
+    BLOCK_SIZE = 3
+    has_read = 0
+    while True:
+        curr_out = p.stdout.read(BLOCK_SIZE)
+        current_read = len(curr_out)
+        has_read+= current_read
+        if curr_out == '': return ''
+        if has_read > range_from:
+            return curr_out[range_from % BLOCK_SIZE:]
 
-def restore_backup (hostname, share, path, restore_host='',backupnumber=-1):
+
+#Example url
+#http://10.0.10.45/index.cgi?action=RestoreFile&host=serversql&num=47&share=/cygdrive/c/db-dump&dir=/TEST_backup_2017_09_13_161329_0384659.fbak
+#
+def get_backuppc_url(hostname, backupnumber, share, path, action = 'RestoreFile', username = 'admin', password = '', instance_url = ''):
+    kwargs = {
+        'hostname' : hostname, 
+        'backupnumber' : backupnumber, 
+        'share' : share, 
+        'path' : path, 
+        'action' : action, 
+        'username' : username,
+    }
+
+    kwargs['password'] = password or __salt__['pillar.get']('admin_password')
+    kwargs['instance_url'] = instance_url or __salt__['network.ip_addrs']()[0]
+
+    url = 'http://{username}:{password}@{instance_url}/index.cgi?action={action}&host={hostname}&num={backupnumber}&share={share}&dir={path}'
+
+    url = url.format(**kwargs)
+    return url
+
+
+def download_zip(hostname, backupnumber = -1, share = '', path = '.', range_from = 0):
+    zip_create_cmd = '/usr/share/backuppc/bin/BackupPC_zipCreate'
+    p = subprocess.Popen(['sudo', '-u', 'backuppc', zip_create_cmd, '-h', hostname, '-n', str(backupnumber),
+        '-s', share, ' ', path], stdout=subprocess.PIPE)
+    BLOCK_SIZE = 10 ** 6
+    has_read = 0
+    while True:
+        curr_out = p.stdout.read(BLOCK_SIZE)
+        current_read = len(curr_out)
+        has_read+= current_read
+        if curr_out == '': return ''
+        if has_read > range_from:
+            return curr_out[range_from % BLOCK_SIZE:]
+
+def restore_backup(hostname, backupnumber = -1, share = '',  path = '.', restore_host=''):
     if restore_host == '':
         restore_host=hostname
     tar_create_cmd = '/usr/share/backuppc/bin/BackupPC_tarCreate'
     args = ' -h '+hostname+' -n '+str(backupnumber)+' -s '+share+' '+path+' | ssh root@'+restore_host+' tar xf - -C '+share
     return __salt__['cmd.run'](tar_create_cmd+args,runas='backuppc', cwd = '/usr/lib/backuppc/',python_shell=True)
 
-def restore(arguments, restore_host='', backupnumber=-1):
-    hostname = arguments[0]
-    share = arguments[1]
-    path = arguments[2]
-    return restore_backup(hostname, share, path, restore_host, backupnumber)
+#This is a temporary 'hack', until we figure out what we really want to do. 
+restore = restore_backup
+
+#def restore(arguments, restore_host='', backupnumber=-1):
+#    hostname = arguments[0]
+#    share = arguments[1]
+#    path = arguments[2]
+#    return restore_backup(hostname, share, path, restore_host, backupnumber)
 
 def infodict(path):
     contents = ''
