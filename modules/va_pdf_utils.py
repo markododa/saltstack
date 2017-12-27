035406593e639ef9d9306b97766b1ddc4ece487d
staci <staci@va.mk>
2017-12-27 14:08:22 +0000

    get_pdf now works with filter_field.

--- modules/va_pdf_utils.py
+++ modules/va_pdf_utils.py
@@ -1,13 +1,16 @@
 import salt, subprocess, json, importlib, sys, os
 
-from reportlab.platypus import Table, TableStyle, Paragraph, Spacer, SimpleDocTemplate
+try:
+    from reportlab.platypus import Table, TableStyle, Paragraph, Spacer, SimpleDocTemplate
 
-from reportlab.lib.enums import TA_LEFT, TA_CENTER
-from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from reportlab.lib.pagesizes import letter, inch
-from reportlab.lib import colors
+    from reportlab.lib.enums import TA_LEFT, TA_CENTER
+    from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+    from reportlab.lib.pagesizes import letter, inch
+    from reportlab.lib import colors
 
-from reportlab.rl_config import defaultPageSize  
+    from reportlab.rl_config import defaultPageSize  
+except: 
+    pass #TODO check if executing on va_master and report error if so
 
 def pdf_styles():
     styles = {
@@ -52,7 +55,7 @@ def pdf_styles():
 
     return styles
 
-def get_pdf(panel, pdf_file = '/tmp/table.pdf', range_from = 0):
+def get_pdf(panel, pdf_file = '/tmp/table.pdf', range_from = 0, filter_field = ''):
     if type(panel) == str: panel = json.loads(panel)
     if range_from: return
 
@@ -61,7 +64,9 @@ def get_pdf(panel, pdf_file = '/tmp/table.pdf', range_from = 0):
         'tables' : [],
     }
     for element in panel['content']:
+        print ('Doing element: ', element)
         if element.get('type', '') != 'Table' :
+            print ('Element is not table : ', element.get('type'), element.keys(), element)
             continue
         panel_table = element
         table = panel['tbl_source'][panel_table['name']]
@@ -70,14 +75,17 @@ def get_pdf(panel, pdf_file = '/tmp/table.pdf', range_from = 0):
         pdf_contents['tables'].append({'table' : table, 'name' : panel_table['name'], 'columns' : panel_table['columns']})
 
 
-    elements = contents_to_elements(pdf_contents, pdf_file)
+    elements = contents_to_elements(pdf_contents, pdf_file, filter_field = filter_field)
  
     doc = SimpleDocTemplate(pdf_file, pagesize=letter)
 
     doc.build(elements)
   
 
-def contents_to_elements(pdf_contents, pdf_file):
+def row_contains(row, filter_field):
+    return any([filter_field in row[x] for x in row])
+
+def contents_to_elements(pdf_contents, pdf_file, filter_field):
     PAGE_HEIGHT=defaultPageSize[1]  
     PAGE_WIDTH=defaultPageSize[0]  
 
@@ -100,12 +108,14 @@ def contents_to_elements(pdf_contents, pdf_file):
             for x in row: 
                 row[x] = str(row[x]) #TODO properly convert lists to string
 
-            row = [Paragraph(row[i['key']], styles['default']) for i in columns]
-            data.append(row)
+            if not filter_field or row_contains(row, filter_field):
+                data_row = [Paragraph(row[i['key']], styles['default']) for i in columns]
+                data.append(data_row)
 
         cols_width = table_width / len(columns)
 
         pdf_table = Table(data, colWidths = cols_width)
+
         pdf_table.setStyle(TableStyle([('INNERGRID', (0,0), (-1,-1), 0.25, colors.black), ('BOX', (0,0), (-1,-1), 0.25, colors.black)]))
 
 #        elements.append(Paragraph(table['name'], styles['Normal']))
