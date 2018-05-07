83342ccb43154d4172522fe02a5dc8500d5ed907
Vlatko <vlatko-va@users.noreply.github.com>
2018-05-07 11:19:05 +0200

    add artful as minion platfrom

--- minion.sh
+++ minion.sh
@@ -19,9 +19,9 @@ fi
 
 version=$(lsb_release -cs)
 
-if [ $version != "jessie" ] && [ $version != "xenial" ] && [ $version != "stretch" ]; then
-	echo "OS not supported"
-	false
+if [ $version != "artful" ] && [ $version != "jessie" ] && [ $version != "xenial" ] && [ $version != "stretch" ]; then
+        echo "OS not supported"
+        false
 fi
 
 if [ $version == "jessie" ]; then
@@ -35,6 +35,11 @@ echo 'deb http://repo.saltstack.com/apt/debian/9/amd64/latest stretch main' > /e
 fi
 
 if [ $version == "xenial" ]; then
+wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | apt-key add -
+echo 'deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main' > /etc/apt/sources.list.d/salt.list
+fi
+
+if [ $version == "artful" ]; then
 wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | apt-key add -
 echo 'deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main' > /etc/apt/sources.list.d/salt.list
 fi
