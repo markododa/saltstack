355ce54a26c6cb29df2db95952c1a3bdf4caf55c
root <staci@va.mk>
2018-05-13 20:02:02 +0200

    gpo fix

--- modules/va_directory/va_directory.py
+++ modules/va_directory/va_directory.py
@@ -95,12 +95,12 @@ def panel_list_organizational_units():
     return org_units
 
 def panel_get_pass_settings():
-    pass_settings = output_to_dict(samba_tool(['domain', 'passwordsettings', 'show']).split('\n')[1:])
+    pass_settings = output_to_dict(samba_tool(['domain', 'passwordsettings', 'show']))
     pass_settings = [{'key' : x, 'value'  : pass_settings[x]} for x in pass_settings]
     return pass_settings
 
 def panel_level_show():
-    res = output_to_dict(samba_tool(['domain', 'level', 'show']).split('\n')[1:])
+    res = output_to_dict(samba_tool(['domain', 'level', 'show']))
     res = [{'key' : x, 'value'  : res[x]} for x in res]
     return res
 
@@ -122,10 +122,9 @@ def panel_get_dc_info():
     return res
 
 def panel_gpo_polices():
-    #THIS NEED a FIX, The Last GPO is only listed
-    res = output_to_dict(samba_tool(['gpo', 'listall']).split('\n'))
-    #res['gpo'] = res.pop('GPO')
-    return [res]
+    res = output_to_dict(samba_tool(['gpo', 'listall']))
+    res = [{'gpo' : r.pop('GPO'), 'display name':  r['display name']} for r in res]
+    return res
 
 def panel_fsmo_show():
     res = samba_tool(['fsmo', 'show']).split('\n')
