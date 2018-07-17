af2f0612dfe93291ff78f56fb6056a17aedfb363
root <staci@va.mk>
2018-07-17 13:09:44 +0200

    Modify chkslt

--- chkslt.sh
+++ chkslt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 git pull
-declare -a git_folders=(states modules reactor)
-declare -a folders=(/srv/salt /srv/salt/_modules /srv/reactor)
+declare -a git_folders=(salt reactor)
+declare -a folders=(/srv/salt /srv/reactor)
 
-for i in $(seq 0 2)
+for i in $(seq 0 1)
 	do diff --brief -r ${git_folders[i]} ${folders[i]} -x _modules
 done
