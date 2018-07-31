cf9293582665c573ea6c890dc1495cc58b5fb174
root <root@va-master.domejn.mk>
2018-07-31 20:22:26 +0200

    Run state on bootstrap-va_master

--- bootstrap-va_master.sh
+++ bootstrap-va_master.sh
@@ -46,4 +46,4 @@ echo "role: va-master" > /etc/salt/grains
 service salt-minion restart
 sleep 30
 salt-key -y -A
-#tail -f -q /var/log/salt/minion |GREP_COLOR='1;32' grep -o "Completed state.*$" --color=always & salt-call --local state.highstate --log-file-level all -l quiet > /dev/null && pkill -f "tail -f -q /var/log/salt/minion"
+tail -f -q /var/log/salt/minion |GREP_COLOR='1;32' grep -o "Completed state.*$" --color=always & salt-call --local state.highstate --log-file-level all -l quiet > /dev/null && pkill -f "tail -f -q /var/log/salt/minion"
