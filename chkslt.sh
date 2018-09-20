49b1739b963dbd5f3417b9f1722fe4ad0d8813d1
root <root@demo.domejn.mk>
2018-09-20 19:14:00 +0200

    fix ssh key acceptance fo

--- chkslt.sh
+++ chkslt.sh
@@ -4,5 +4,5 @@ declare -a git_folders=(salt reactor)
 declare -a folders=(/srv/salt /srv/reactor)
 
 for i in $(seq 0 1)
-	do diff --brief -r ${git_folders[i]} ${folders[i]} -x _modules
+	do diff --brief -r ${git_folders[i]} ${folders[i]}
 done
