f45520928fa32b9f3d17128c4a8cae8c05c52ec2
Marko Doda <marko@lugola.net>
2019-04-21 23:45:15 +0200

    use delete instead of exec rm for find

--- salt/base/files/db-backup.sh
+++ salt/base/files/db-backup.sh
@@ -4,10 +4,10 @@ CRED="-uroot"
 DUMP="mysqldump $CRED --ignore-table=mysql.event"
 PATEKA="/root/.va/backup"
 DATE=`date +%Y%m%d-%H%M`
-find $PATEKA -mtime +180 -exec rm {} \;
+find $PATEKA -iname *.gz -mtime +180 -delete;
 
 # Get a list of all databases
-DATABASES=$(echo "SHOW DATABASES" | mysql $CRED | grep -v Database | grep -v performance_schema | grep -v information_schema)
+DATABASES=$(echo "SHOW DATABASES" | mysql $CRED | grep -v -E "Database|performance_schema|information_schema")
 
 for DB  in $DATABASES; do
         FILENAME="$PATEKA/$DB-$DATE.sql.gz"
