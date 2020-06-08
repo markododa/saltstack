a59b338bc4006723d13c65103ece4287c98778b7
NinoDoko <ninodokoskiot@hotmail.com>
2020-06-08 14:55:48 +0000

    change conflict resolution

--- salt/_modules/va_email/vcards.py
+++ salt/_modules/va_email/vcards.py
@@ -7,7 +7,7 @@ status_path = "~/.vdirsyncer/status/"
 [pair {account_name}]
 a = "{account_name}_folder"
 b = "{account_name}_remote"
-conflict_resolution = "b wins"
+conflict_resolution = "a wins"
 collections = ["from a", "from b"]
 
 [storage {account_name}_folder]
